fd4bf3d12e15ae59a4c8484dd1e7732c25a4390e
Anisa <aagermanova@gmail.com>
2020-02-27 20:00:18 -0800

    2 step

--- js/profile.js
+++ js/profile.js
@@ -1,18 +1,13 @@
-(function(){
-  var user = {
-    name : "Shekhar Gulati",
-    messages : [
-      "hello",
-      "bye",
-      "good night"
-    ]
-  };
+var timeline = require('./timeline.js');
+var user = {
+  name : "Shekhar Gulati",
+  messages : [
+    "hello",
+    "bye",
+    "good night"
+  ]
+};
 
-  $("#timeline").text(user.name+ " Timeline");
-
-  _.each(user.messages, function(msg){
-    var html = "<li><div class='timeline-heading'><h4 class='timeline-title'>"+msg+"</h4></div></li>";
-    $(".timeline").append(html);
-  });
-
-}());
+var timelineModule = new timeline(user);
+timelineModule.setHeader(user);
+timelineModule.setTimeline(user);
